7a07073f9ad2ba1858e366d2863c082e4705396b
ldjtun <158047177+ldjtun@users.noreply.github.com>
2024-02-28 19:24:34 +0800

    Create test.java
    java代码

---
+++ test.java
@@ -0,0 +1,24 @@
+public class BubbleSort {
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Swap arr[j] and arr[j+1]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 8, 12, 1};
+        bubbleSort(arr);
+        System.out.println("Sorted array:");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
